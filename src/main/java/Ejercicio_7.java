b394dc2ea1e2d1f99a982e3b7bd0794910e8f8f6
nquinbla <nquinbla@myuax.com>
2024-02-07 19:20:17 +0100

    bu

--- src/main/java/Ejercicio_7.java
+++ src/main/java/Ejercicio_7.java
@@ -1,2 +1,23 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Ejercicio_7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce un número entero:");
+        int number = scanner.nextInt();
+        ArrayList<Integer> factors = primeFactors(number);
+        System.out.println("Los factores primos de " + number + " son: " + factors);
+    }
+
+    public static ArrayList<Integer> primeFactors(int number) {
+        ArrayList<Integer> factors = new ArrayList<>();
+        for (int i = 2; i <= number; i++) {
+            while (number % i == 0) {
+                factors.add(i);
+                number /= i;
+            }
+        }
+        return factors;
+    }
 }
