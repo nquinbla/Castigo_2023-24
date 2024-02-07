0a001c34e277affe55d4632dd8bdb8906f704332
nquinbla <nquinbla@myuax.com>
2024-02-07 18:02:33 +0100

    bu

--- src/main/java/Ejercicio_1.java
+++ src/main/java/Ejercicio_1.java
@@ -5,10 +5,21 @@ public class Ejercicio_1 {
         short minutesInHour = 60;
         short secondsInMinute = 60;
 
+        int years = 1;
+        int months = 1;
+        int days = 1;
+
+        long totalSeconds = years * daysInYear * hoursInDay * minutesInHour * secondsInMinute;
+        System.out.println("Total seconds in " + years + " year: " + totalSeconds);
 
     }
 
-
-
+    public static long calculateSeconds(int years, int months, int days, int hours, int minutes, int daysInYear, short hoursInDay, short minutesInHour, short secondsInMinute) {
+        long totalDays = years * daysInYear + months * (daysInYear / 12) + days;
+        long totalHours = totalDays * hoursInDay;
+        long totalMinutes = totalHours * minutesInHour;
+        long totalSeconds = totalMinutes * secondsInMinute;
+        return totalMinutes * secondsInMinute;
+    }
 }
 
