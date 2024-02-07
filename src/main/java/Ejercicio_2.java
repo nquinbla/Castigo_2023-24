ed733fe00e29235344df525685223442ca8615b6
nquinbla <nquinbla@myuax.com>
2024-02-07 18:17:32 +0100

    bu

---
+++ src/main/java/Ejercicio_2.java
@@ -0,0 +1,6 @@
+public class Ejercicio_2 {
+    public static void main(String[] args) {
+        System.out.println("El mayor número que se puede representar con un char es: " + (int) Character.MAX_VALUE);
+
+    }
+}
