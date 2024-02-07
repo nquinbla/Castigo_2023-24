acc577fc3d33a1687a964c5c96f310cce26d1a00
nquinbla <nquinbla@myuax.com>
2024-02-07 18:19:31 +0100

    bu

--- src/main/java/Ejercicio_2.java
+++ src/main/java/Ejercicio_2.java
@@ -1,6 +1,8 @@
 public class Ejercicio_2 {
     public static void main(String[] args) {
         System.out.println("El mayor número que se puede representar con un char es: " + (int) Character.MAX_VALUE);
+        System.out.println("El mayor número que se puede representar con un short es: " + Short.MAX_VALUE);
+        System.out.println("El mayor número que se puede representar con un int es: " + Integer.MAX_VALUE);
 
     }
 }
