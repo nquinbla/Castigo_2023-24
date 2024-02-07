770236d06ecec460c724cc8c4da5d6d8c1ffd57f
nquinbla <nquinbla@myuax.com>
2024-02-07 17:56:50 +0100

    bu

--- src/main/java/Ejercicio_1.java
+++ src/main/java/Ejercicio_1.java
@@ -1,6 +1,11 @@
 public class Ejercicio_1 {
     public static void main(String[] args) {
         short daysInYear = 365;
+        short hoursInDay = 24;
+        short minutesInHour = 60;
+        short secondsInMinute = 60;
+
+
     }
 
 
